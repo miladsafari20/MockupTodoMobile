d77fb9464b7f47d39e8b40087d2580e08e41cae7
Milad Safari <miladhajisafari72@gmail.com>
2024-01-11 17:24:17 +0330

    init

---
+++ app.js
@@ -0,0 +1,20 @@
+// let people = {
+//    name : "milad" , 
+//   family: "safari" , 
+//    age : 22,
+// }
+
+
+// const seter =  (key_Name , value_Name) =>{
+//    localStorage.setItem(key_Name , JSON.stringify(value_Name))
+// }
+// const geter = (key_Name) =>{
+//    let get_Obj = localStorage.getItem(key_Name)
+//    let  Obj_string = JSON.parse(get_Obj)
+//    console.log(Obj_string)
+// }
+
+// seter("test" , people)
+// geter("test")
+
+// localStorage.clear() 
